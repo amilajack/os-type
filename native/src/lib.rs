f72746861ff86386226da6b79fb388c48aa3d630
Amila Welihinda <amilajack@gmail.com>
2018-10-21 22:53:42 -0700

    return obj in js

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -4,9 +4,28 @@ extern crate os_type;
 
 use neon::prelude::*;
 
-fn current_platform(mut cx: FunctionContext) -> JsResult<JsString> {
+fn map_os_type_to_string<'a>(os_type: os_type::OSType) -> &'a str {
+    match os_type {
+        Unknown => "unknown",
+        Redhat => "redhat",
+        OSX => "osx",
+        Ubuntu => "ubuntu",
+        Debian => "debian",
+        Arch => "arch",
+        Manjaro => "manjaro",
+        CentOS => "centos",
+        OpenSUSE => "opensuse"
+    }
+}
+
+fn current_platform(mut cx: FunctionContext) -> JsResult<JsObject> {
     let os = os_type::current_platform();
-    Ok(cx.string(format!("{} {:?}", os.version, os.os_type)))
+    let object = JsObject::new(&mut cx);
+    let os_type_js_string = cx.string(map_os_type_to_string(os.os_type));
+    let js_string_version = cx.string(os.version);
+    object.set(&mut cx, "version", js_string_version).unwrap();
+    object.set(&mut cx, "osType", os_type_js_string).unwrap();
+    Ok(object)
 }
 
 register_module!(mut cx, {
